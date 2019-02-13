2879d4cc8e9522ff2ea8eb38e22987beb98bd9f8
fanyeqing <fanyeqing@bilibili.com>
2019-02-13 18:49:04 +0800

    删除不必要的

--- miniprogram/components/create/create.js
+++ miniprogram/components/create/create.js
@@ -3,8 +3,10 @@ Component({
         addGlobalClass: true
     },
     data: {
+        listViewH: 'auto',
         shown: false,
         anime: false,
+        inputShow: false,
         tabs: [{
             value: 0,
             label: '收入'
@@ -12,7 +14,13 @@ Component({
             value: 1,
             label: '支出'
         }],
-        current: 0
+        current: 0,
+        categories: [],
+        select: {},
+        inputAction: '',
+        input: {
+            summary: '0.00'
+        }
     },
     /**
      * 组件的属性列表
@@ -30,6 +38,7 @@ Component({
                         this.setData({
                             anime: newValue
                         });
+                        this.setViewHeight();
                         clearTimeout(timer);
                     }, 50);
                 } else {
@@ -38,7 +47,10 @@ Component({
                     });
                     let timer = setTimeout(() => {
                         this.setData({
-                            shown: newValue
+                            shown: newValue,
+                            select: {},
+                            inputShow: false,
+                            listViewH: 'auto'
                         });
                         clearTimeout(timer);
                     }, 300);
@@ -46,10 +58,47 @@ Component({
             }
         }
     },
+    lifetimes: {
+        attached() {
+            this.getCategories();
+        },
+        detached() {}
+    },
     /**
      * 组件的方法列表
      */
     methods: {
+        setViewHeight() {
+            let query = this.createSelectorQuery();
+            let className = this.data.inputShow ? '.category-list-small' : '.category-list-large';
+            query.select(className).boundingClientRect().exec(res => {
+                let rect = res[0];
+                if (rect) {
+                    this.setData({
+                        listViewH: `${rect.height}px`
+                    });
+                }
+            })
+        },
+        getCategories() {
+            const db = wx.cloud.database()
+            // 查询所有类别
+            db.collection('categories').where({
+                type: this.data.tabs[this.data.current].value
+            }).get({
+                success: res => {
+                    this.setData({
+                        categories: res.data
+                    })
+                },
+                fail: err => {
+                    wx.showToast({
+                        icon: 'none',
+                        title: '分类获取失败'
+                    })
+                }
+            })
+        },
         close() {
             this.triggerEvent('close');
         },
@@ -58,6 +107,52 @@ Component({
             this.setData({
                 current: item.value
             });
+            this.getCategories();
+        },
+        selectCategory(e) {
+            let item = e.currentTarget.dataset.item;
+            this.setData({
+                select: item,
+                inputShow: true
+            });
+            this.setViewHeight();
+        },
+        keyBoardPress(e) {
+            let value = e.currentTarget.dataset.value;
+            if (value === 'date') {
+
+            } else if (value === 'del') {
+                let input = this.data.input;
+                input.action = value;
+                this.setData({
+                    input: input
+                });
+            } else if (value === 'add') {
+                let input = this.data.input;
+                input.action = value;
+                this.setData({
+                    input: input
+                });
+            } else if (value === 'min') {
+                let input = this.data.input;
+                input.action = value;
+                this.setData({
+                    input: input
+                });
+            } else {
+                let summary = '';
+                if (this.data.input.summary === '0.00' || this.data.input.summary === '0') {
+                    summary = value;
+                } else {
+                    summary = this.data.input.summary + value;
+                }
+                this.setData({
+                    input: {
+                        action: '',
+                        summary: summary
+                    }
+                });
+            }
         }
     }
 })
--- miniprogram/pages/home/home.js
+++ miniprogram/pages/home/home.js
@@ -2,6 +2,8 @@ Page({
     data: {
         year: '',
         month: '',
+        pickerArray: [['2019'], ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']],
+        pickerSelect: [0, 0],
         count: {
             inCount: '0.00',
             outCount: '0.00'
@@ -9,16 +11,30 @@ Page({
         shown: false
     },
     onLoad(option) {
+        let date = new Date();
+        let year = date.getFullYear().toString();
+        let month = date.getMonth() + 1;
+        if (month < 10) month = `0${month}`;
+        else month = month.toString();
+        let select = [this.data.pickerArray[0].indexOf(year), this.data.pickerArray[1].indexOf(month)];
         this.setData({
-            year: '2019年',
-            month: '02',
+            pickerSelect: select,
+            year: `${this.data.pickerArray[0][select[0]]}年`,
+            month: this.data.pickerArray[1][select[1]],
             count: {
                 inCount: '2.00',
                 outCount: '3.02'
             }
         })
     },
-    showPicker(e) {},
+    pickerChange(e) {
+        let value = e.detail.value;
+        this.setData({
+            pickerSelect: value,
+            year: `${this.data.pickerArray[0][value[0]]}年`,
+            month: this.data.pickerArray[1][value[1]]
+        })
+    },
     create() {
         this.setData({
             shown: true
